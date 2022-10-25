d152e019bf8acf0723fe3196921f6a565e6c5838
MoonTurtlee <joaquin.dominguez@usm.cl>
2022-10-25 17:40:51 -0300

    En el dashboard ahora aparece un diccionario feo 	new file:   SansaNews/Iniciativas/gbu_usm/2022-04-19_04-58-33_UTC.json 	new file:   SansaNews/Iniciativas/gbu_usm/2022-04-19_04-59-02_UTC.json 	new file:   SansaNews/Iniciativas/gbu_usm/2022-04-19_04-59-24_UTC.json 	new file:   SansaNews/Iniciativas/gbu_usm/2022-06-02_17-26-23_UTC.json 	new file:   SansaNews/Iniciativas/gbu_usm/2022-06-23_01-34-54_UTC.json 	new file:   SansaNews/Iniciativas/gbu_usm/2022-06-27_15-13-11_UTC.json 	new file:   SansaNews/Iniciativas/gbu_usm/2022-07-09_01-37-28_UTC.json 	new file:   SansaNews/Iniciativas/gbu_usm/2022-08-24_03-31-08_UTC.json 	new file:   SansaNews/Iniciativas/gbu_usm/2022-09-29_12-41-51_UTC.json 	new file:   SansaNews/Iniciativas/gbu_usm/gbu_usm_52691001886.json 	new file:   SansaNews/Iniciativas/gbu_usm/id 	new file:   SansaNews/__pycache__/json_reader.cpython-310.pyc 	modified:   SansaNews/__pycache__/views.cpython-310.pyc 	new file:   SansaNews/gbu_usm/2022-04-19_04-58-33_UTC.json 	new file:   SansaNews/gbu_usm/2022-04-19_04-59-02_UTC.json 	new file:   SansaNews/gbu_usm/2022-04-19_04-59-24_UTC.json 	new file:   SansaNews/gbu_usm/2022-06-02_17-26-23_UTC.json 	new file:   SansaNews/gbu_usm/2022-06-23_01-34-54_UTC.json 	new file:   SansaNews/gbu_usm/2022-06-27_15-13-11_UTC.json 	new file:   SansaNews/gbu_usm/2022-07-09_01-37-28_UTC.json 	new file:   SansaNews/gbu_usm/2022-08-24_03-31-08_UTC.json 	new file:   SansaNews/gbu_usm/2022-09-29_12-41-51_UTC.json 	new file:   SansaNews/gbu_usm/gbu_usm_52691001886.json 	new file:   SansaNews/gbu_usm/id 	modified:   SansaNews/json_reader.py 	modified:   SansaNews/templates/dashboard.html 	modified:   SansaNews/views.py 	new file:   gbu_usm/2022-04-19_04-58-33_UTC.json 	new file:   gbu_usm/2022-04-19_04-59-02_UTC.json 	new file:   gbu_usm/2022-04-19_04-59-24_UTC.json 	new file:   gbu_usm/2022-06-02_17-26-23_UTC.json 	new file:   gbu_usm/2022-06-23_01-34-54_UTC.json 	new file:   gbu_usm/2022-06-27_15-13-11_UTC.json 	new file:   gbu_usm/2022-07-09_01-37-28_UTC.json 	new file:   gbu_usm/2022-08-24_03-31-08_UTC.json 	new file:   gbu_usm/2022-09-29_12-41-51_UTC.json 	new file:   gbu_usm/gbu_usm_52691001886.json 	new file:   gbu_usm/id

--- SansaNews/json_reader.py
+++ SansaNews/json_reader.py
@@ -6,58 +6,65 @@ from os import path
 from os.path import isfile,join,isdir
 
 def leer_json(archivo):
-    archivo_json = open(archivo)
-    #Convertimos el JSON a diccionario.
+
     if "json" not in archivo:
-        return 0
+        pagina = "Null"
+        lista = ["vacia"]
+        return pagina,lista
+    #Convertimos el JSON a diccionario.
+    archivo_json = open(archivo)
     data = json.load(archivo_json)
 
+    #obtiene el nombre de la pagina, se crea el diccionario y las listas vacias
+    if "owner" in data['node']:
+        pagina = data['node']["owner"]["username"]
+    else:
+        pagina = data['node']["username"]
+    lista = []
     #Verifica si hay mas URL's
     if "edge_sidecar_to_children" in data["node"]:
         cantidad = len(data['node']["edge_sidecar_to_children"]["edges"])
         contador = 0
         while contador < cantidad:
-            print("Esta es la URL",contador + 1,":",data['node']["edge_sidecar_to_children"]["edges"][contador]["node"]["display_url"],"\n")
+            lista.append(data['node']["edge_sidecar_to_children"]["edges"][contador]["node"]["display_url"])
             contador += 1
     elif "display_url" not in data['node']:
-        print("Final de los links")
+        basura = 0
     else:
-        print("Esta es la única URL:",data['node']["display_url"])
+        lista.append(data['node']["display_url"])
 
     # Lee la descripcion.
     if "edge_media_to_caption" not in data['node']:
-        print("Final de las descripciones\n")
+        basura = 0
     elif len(data['node']["edge_media_to_caption"]["edges"]) == 0:
-        print("No hay descripccion\n")
+        lista.append(["Sin descripcion"])
     else:
-        print("Esta es la descripción:",data['node']["edge_media_to_caption"]["edges"][0]['node']['text'],"\n\n","-----------------------------------------------------------------------------")
+        lista.append(data['node']["edge_media_to_caption"]["edges"][0]['node']['text'])
     #Se cierra el archivo
     archivo_json.close()
-
-#Define la ruta en donde se guardan las carpetas extraidas a partir de instaloader
-nombre= "json_reader.py"
-ruta = path.abspath(nombre)
-lista = ruta.split("\\")
-del lista[-1]
-texto = ""
-for elemento in lista:
-	texto += elemento+"/"
-ruta_carpeta = texto + "{}/"
-
-#Este es el contenido de lo que hay en la ruta texto
-contenido = os.listdir(texto)
-#obtener carpetas
-carpetas = [nombre for nombre in contenido if isdir(join(texto,nombre))]
-contador = 0
-while contador < len(carpetas):
-    texto = ruta_carpeta
-    texto = texto.format(carpetas[contador])
-    #obtener archivos
+    return pagina,lista
+    
+#Este es el contenido de lo que hay en la ruta
+def contenido(texto,ruta_carpeta):
     contenido = os.listdir(texto)
-    archivos = [nombre for nombre in contenido if isfile(join(texto,nombre))]
-    contador_2 = 0
-    while contador_2 < len(archivos):
-        directorio = texto + archivos[contador_2]
-        prints = leer_json(directorio)
-        contador_2 += 1
-    contador += 1
+    #obtener carpetas
+    carpetas = [nombre for nombre in contenido if isdir(join(texto,nombre))]
+    contador = 0
+    dicc = {}
+    while contador < len(carpetas):
+        texto = ruta_carpeta
+        texto = texto.format(carpetas[contador])
+        #obtener archivos
+        contenido = os.listdir(texto)
+        archivos = [nombre for nombre in contenido if isfile(join(texto,nombre))]
+        contador_2 = 0
+        while contador_2 < len(archivos):
+            directorio = texto + archivos[contador_2]
+            pagina,lista = leer_json(directorio)
+            if pagina not in dicc:
+                dicc[pagina]=[lista]
+            else:
+                dicc[pagina]+=[lista]
+            contador_2 += 1
+        contador += 1
+    return dicc
--- SansaNews/views.py
+++ SansaNews/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from . import json_reader
 # Create your views here.
 
 def dashboard(request):
-    return render(request,"dashboard.html")
+    ruta_carpeta = "C:/Users/PlayF/Desktop/App Web/SansaNews/SansaNews/Iniciativas/" + "{}/"
+    texto = "C:/Users/PlayF/Desktop/App Web/SansaNews/SansaNews/Iniciativas/"
+    diccionario = json_reader.contenido(texto,ruta_carpeta)
+    return render(request,"dashboard.html",context={"llave" : diccionario})

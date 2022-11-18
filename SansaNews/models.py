8c60862dfcf6a1696d4e02df19faf440c677e05c
MoonTurtlee <joaquin.dominguez@usm.cl>
2022-11-18 01:54:02 -0300

    modified:   SansaNews/models.py

--- SansaNews/models.py
+++ SansaNews/models.py
@@ -4,7 +4,7 @@ class imagenes_avisos(models.Model):
     descripcion = models.TextField(max_length=400, null=True)
     url = models.FileField(upload_to="Avisos/" ,max_length=200)
     def str(self) -> str:
-        return '{} {}'.format(self.name_image, self.url)
+        return '{} {}'.format(self.descripcion, self.url)
 
     class Meta:
         db_table = 'Avisos'

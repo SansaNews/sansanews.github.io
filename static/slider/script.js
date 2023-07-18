ee42c6ea821d8d6562cb233413e39a5df2e6fa30
MoonTurtlee <joaquin.dominguez@usm.cl>
2023-07-18 19:37:26 -0400

    intento de coloreo

--- static/slider/script.js
+++ static/slider/script.js
@@ -1,4 +1,4 @@
-const slider = document.querySelector(".slider");
+    const slider = document.querySelector(".slider");
     const nextBtn = document.querySelector(".next-btn");
     const prevBtn = document.querySelector(".prev-btn");
     const slides = document.querySelectorAll(".slide");
@@ -44,6 +44,59 @@ const slider = document.querySelector(".slider");
       slideIcons[slideNumber].classList.add("active");
     });
 
+    document.addEventListener('DOMContentLoaded', function() {
+      formatAllTextAndColor();
+    });
+    
+    function formatAllTextAndColor() {
+      const descripciones = document.querySelectorAll('#descripcion');
+    
+      descripciones.forEach(descripcion => {
+        const texto = descripcion.textContent.trim();
+        const bloquesTexto = texto.split(/(?<=\.\s|\uD800[\uDC00-\uDFFF]|\uD83C[\uDF00-\uDFFF]|\uD83D[\uDC00-\uDDFF]|\uD83E[\uDD00-\uDDFF])/);
+    
+        descripcion.innerHTML = '';
+    
+        let parrafoActual = null;
+        bloquesTexto.forEach(bloque => {
+          if (bloque !== '') {
+            if (bloque.match(/[\uD800-\uDFFF]|[\uD83C-\uD83E][\uDC00-\uDFFF]/)) {
+              if (parrafoActual) {
+                descripcion.appendChild(parrafoActual);
+              }
+              parrafoActual = document.createElement('p');
+              parrafoActual.innerHTML = bloque;
+              descripcion.appendChild(parrafoActual);
+            } else if (bloque.startsWith('. ')) {
+              parrafoActual.innerHTML += bloque;
+            } else {
+              const updatedBloque = bloque.replace(/(@\w+|#\w+)/g, '<span class="special-char">$1</span>');
+              const ocurrencias = updatedBloque.match(/(@\w+|#\w+)/g);
+              if (ocurrencias) {
+                if (parrafoActual) {
+                  descripcion.appendChild(parrafoActual);
+                }
+                parrafoActual = document.createElement('p');
+                ocurrencias.forEach(ocurrencia => {
+                  updatedBloque = updatedBloque.replace(ocurrencia, `<span class="special-char">${ocurrencia}</span>`);
+                });
+                parrafoActual.innerHTML = updatedBloque;
+                descripcion.appendChild(parrafoActual);
+              } else {
+                if (!parrafoActual) {
+                  parrafoActual = document.createElement('p');
+                  descripcion.appendChild(parrafoActual);
+                }
+                parrafoActual.innerHTML += bloque;
+              }
+            }
+          }
+        });
+        if (parrafoActual) {
+          descripcion.appendChild(parrafoActual);
+        }
+      });
+    }
     //image slider autoplay
     //var playSlider;
     /*

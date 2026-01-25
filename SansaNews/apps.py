792ec0c538c4d7042e8298bc94e5d0a38d5f1d27
Joaquín <dominguezbustosjoaquin@gmail.com>
2026-01-25 01:17:13 -0300

    NewMain

--- SansaNews/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SansanewsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SansaNews'
--- SansaNews/forms.py
+++
@@ -1,9 +0,0 @@
-from . import models
-from django.contrib.auth.forms import UserCreationForm, forms
-
-
-class avisos_forms(forms.ModelForm):
-    class Meta:
-        model = models.imagenes_avisos
-        fields = ["descripcion","url"]
-        labels = {"url": "", "descripcion": ""}

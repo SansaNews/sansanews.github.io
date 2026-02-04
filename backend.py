8051467d4452ac7557511f5a2227c9ab6ddf0b0e
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2026-02-04 19:17:08 -0300

    refactor: funcion main

--- backend.py
+++ backend.py
@@ -8,6 +8,28 @@ import requests
 from dotenv import load_dotenv
 
 
+def main():
+    ACTIONS = ["check", "get"]
+    API_VERSION = "v24.0"
+    INSTAGRAM_ID = "17841455210130919"
+    API_URL = f"https://graph.facebook.com/{API_VERSION}/{INSTAGRAM_ID}"
+
+    action = sys.argv[1]
+    assert action in ACTIONS, f"{action} not in available actions"
+
+    access_token = os.getenv("ACCESS_TOKEN", "")
+    assert access_token, "access token is empty"
+
+    if action == "check":
+        username = sys.argv[2]
+        is_business_account = check_if_business_account(username, API_URL, access_token)
+        print(is_business_account)
+    elif action == "get":
+        username = sys.argv[2]
+        data = get_account_posts(username, API_URL, access_token)
+        pprint(data)
+
+
 def check_if_business_account(username: str, url: str, access_token: str) -> bool:
     fields = f"business_discovery.username({username}){{name}}"
     params = {"fields": fields, "access_token": access_token}
@@ -44,22 +66,5 @@ def get_account_posts(
 
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s: [%(levelname)s] %(message)s")
-
-    API_VERSION = "v24.0"
-    INSTAGRAM_ID = "17841455210130919"
-    API_URL = f"https://graph.facebook.com/{API_VERSION}/{INSTAGRAM_ID}"
-
     _ = load_dotenv()
-    access_token = os.getenv("ACCESS_TOKEN", "")
-    assert access_token, "access token is empty"
-
-    action = sys.argv[1]
-
-    if action == "check":
-        username = sys.argv[2]
-        is_business_account = check_if_business_account(username, API_URL, access_token)
-        print(is_business_account)
-    if action == "get":
-        username = sys.argv[2]
-        data = get_account_posts(username, API_URL, access_token)
-        pprint(data)
+    main()

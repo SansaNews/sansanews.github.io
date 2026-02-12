90d2b91e4c0661c7437d4a810602c46d743f3073
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2026-02-12 17:49:59 -0300

    fix: images maintain aspect ratio

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -3,44 +3,44 @@
   import { type Media } from "$lib/types";
   import User from "$lib/components/User.svelte";
 
+  const MEDIA_WIDTH = 96;
   let media: Media = $props();
 </script>
 
 <Card.Root class="mb-8 border-0 bg-transparent shadow-none">
   <Card.Content class="px-0">
     <div
-      class="bg-card card-shadow flex flex-col overflow-hidden rounded-lg border-2 border-t md:h-82 md:flex-row"
+      class="bg-card card-shadow relative flex flex-col overflow-hidden rounded-lg border-2 border-t md:flex-row"
     >
-      <div class="aspect-square border-b-2 md:border-r-2 md:border-b-0">
+      <div
+        class="border-b-2 md:w-{MEDIA_WIDTH} md:shrink-0 md:border-r-2 md:border-b-0"
+      >
         <!-- Post image -->
-        <a
-          href={media.permalink}
-          target="_blank"
-          rel="noopener noreferrer"
-          class="relative block h-full"
-        >
+        <a href={media.permalink} target="_blank" rel="noopener noreferrer">
           {#if media.type === "VIDEO"}
             <video
               src={media.url}
               muted
               loop
               autoplay
-              class="h-full w-full object-cover object-center"
+              class="h-auto w-full object-contain"
             ></video>
           {:else}
             <img
               src={media.url}
               alt="Post de {media.username}"
               referrerpolicy="no-referrer"
-              class="h-full w-full object-cover"
+              class="h-auto w-full object-contain"
             />
           {/if}
         </a>
       </div>
 
-      <div class="flex grow flex-col p-4">
+      <div
+        class="flex w-full flex-col justify-between p-4 md:absolute md:inset-y-0 md:right-0 md:left-{MEDIA_WIDTH} md:w-auto"
+      >
         <!-- Description -->
-        <p class="grow overflow-y-auto wrap-break-word whitespace-pre-line">
+        <p class="overflow-y-auto wrap-break-word whitespace-pre-line">
           {media.caption}
         </p>
 

854a21ae06f1fb81bf30c28ea5d27a15128af43e
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2026-02-23 19:46:21 -0300

    feat: separación de fechas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import "../app.css";
-  import * as Card from "$lib/components/ui/card/index.js";
   import Navbar from "$lib/components/navbar/Navbar.svelte";
   import Footer from "$lib/components/Footer.svelte";
 
@@ -9,35 +8,10 @@
 
 <div class="grid grid-cols-12">
   <header class="col-span-12 pb-4 lg:col-span-8 lg:col-start-3">
-    <div class="lg:px-4 lg:py-8">
+    <div class="lg:px-4 lg:pt-8 lg:pb-6">
       <Navbar />
     </div>
   </header>
-
-  <!-- Banner -->
-  <div class="col-span-12 px-4 lg:col-span-8 lg:col-start-3">
-    <Card.Root class="gap-0 border-0 py-0">
-      <Card.Header class="bg-foreground text-background rounded-t-lg py-2">
-        <Card.Title
-          class="text-center text-sm font-bold tracking-widest uppercase"
-        >
-          <p>beta-1.0.0</p>
-        </Card.Title>
-      </Card.Header>
-      <Card.Content class="p-4 text-center">
-        <h2 class="mb-2 font-[--font-heading] text-xl font-bold lg:text-2xl">
-          WORK IN PROGRESS
-        </h2>
-        <p class="text-muted-foreground text-xs tracking-wide sm:text-sm">
-          SansaNews se encuentra en la fase inicial de desarrollo, pronto se
-          implementarán secciones nuevas y mejoras en la interfaz.
-        </p>
-        <p class="text-muted-foreground text-xs tracking-wide sm:text-sm">
-          ¡Gracias por ser parte de este proceso!
-        </p>
-      </Card.Content>
-    </Card.Root>
-  </div>
 
   <div class="col-span-12 lg:col-span-8 lg:col-start-3">
     {@render children()}
--- src/routes/[[category]]/+page.svelte
+++ src/routes/[[category]]/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import Post from "$lib/components/Post.svelte";
   import data from "$lib/assets/media.json";
-  import { type Media, jsonToMedia, formatDatetime } from "$lib/media";
+  import {
+    type Media,
+    jsonToMedia,
+    formatDatetime,
+    getTimeCategory,
+  } from "$lib/media";
   import type { PageProps } from "./$types";
   import * as Empty from "$lib/components/ui/empty";
   import { ImageOff } from "@lucide/svelte";
@@ -9,23 +14,38 @@
   let { params }: PageProps = $props();
 
   const allMedia: Media[] = jsonToMedia(data.media);
-  const mediaList = $derived.by(() => {
+
+  let filteredMedia = $derived.by(() => {
     if (params.category) {
       return allMedia.filter((media) => media.category === params.category);
     }
-
     return allMedia;
+  });
+
+  let groupedMedia = $derived.by(() => {
+    let groups: Record<string, Media[]> = {};
+    const order = ["Hoy", "Ayer", "Última Semana", "Último Mes"];
+
+    filteredMedia.forEach((media) => {
+      let category = getTimeCategory(media.datePublished);
+      if (!groups[category]) {
+        groups[category] = [];
+      }
+      groups[category].push(media);
+    });
+
+    return order
+      .filter((key) => groups[key] && groups[key].length > 0)
+      .map((key) => ({ title: key, items: groups[key] }));
   });
 
   let now = $state(new Date());
 
   $effect(() => {
     now = new Date();
-
     const interval = setInterval(() => {
       now = new Date();
     }, 60 * 1000); // Updates every minute
-
     return () => clearInterval(interval);
   });
 </script>
@@ -36,18 +56,28 @@
   </p>
 
   <section>
-    {#each mediaList as media}
-      <Post {...media} />
+    {#if groupedMedia.length > 0}
+      {#each groupedMedia as group}
+        <div class="mt-8 flex w-full items-center justify-center gap-4">
+          <div class="bg-primary/40 h-0.5 w-full"></div>
+          <h2 class="font-heading text-xl whitespace-nowrap">{group.title}</h2>
+          <div class="bg-primary/40 h-0.5 w-full"></div>
+        </div>
+
+        {#each group.items as media}
+          <Post {...media} />
+        {/each}
+      {/each}
     {:else}
-      <Empty.Root class="border border-dashed my-8">
+      <Empty.Root class="my-8 border border-dashed">
         <Empty.Media variant="icon" class="shadow">
           <ImageOff class="h-12 w-12" />
         </Empty.Media>
         <Empty.Title>Sin publicaciones</Empty.Title>
         <Empty.Description>
-          No hay publicaciones nuevas en el último mes.
+          No hay publicaciones disponibles en esta categoría.
         </Empty.Description>
       </Empty.Root>
-    {/each}
+    {/if}
   </section>
 </main>

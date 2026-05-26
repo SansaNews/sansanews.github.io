b6eda7dc757a662c5a33c91066a91b444fa47d88
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2026-05-26 15:02:42 -0400

    fix: height fecha publicacion

--- src/lib/components/User.svelte
+++ src/lib/components/User.svelte
@@ -27,7 +27,7 @@
       </span>
     {:else}
       <div class="flex justify-end pt-1">
-        <Skeleton class="h-2.5 w-16" />
+        <Skeleton class="h-3.25 w-16" />
       </div>
     {/if}
   </div>
